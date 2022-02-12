cd0e49d2edc4d3ce7802e86a39eba4cb8d5635f6
Harry Beechey <harry.beechey@protonmail.com>
2022-02-12 18:21:15 +0000

    Added /NoRestartUI to updateassistant execution

--- Windows/Updates/WindowsUpdateAssistant.ps1
+++ Windows/Updates/WindowsUpdateAssistant.ps1
@@ -4,4 +4,4 @@ if(!(Test-Path "C:\temp")){
 
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?LinkID=799445" -OutFile "C:\temp\updateassistant.exe"
 
-c:\temp\updateassistant.exe /quietinstall /skipeula /auto upgrade
+c:\temp\updateassistant.exe /quietinstall /skipeula /auto upgrade /NoRestartUI
